965fbfa554509268fb359a367b3400b9059d996d
Scott Nicholes <scott93gps@gmail.com>
2026-08-11 11:38:43 -0700

    Commented out data pointer and included vector

--- stack/stack.h
+++ stack/stack.h
@@ -1,10 +1,12 @@
 #ifndef STACK_H
 #define STACK_H
+#include <stack>
 
 class Stack
 {
  private:
-  int* dataPtr; // We wish to model a single access point for the data
+  // int* dataPtr; // We wish to model a single access point for the data
+  std::stack<int> data;
   int topIndex; // We will use an index to access the chunk of data we want
   int size; // Cardinality: How many elements
   int capacity; // Total allocated space for this container
